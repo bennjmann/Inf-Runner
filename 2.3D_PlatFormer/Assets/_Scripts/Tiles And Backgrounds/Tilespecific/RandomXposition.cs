356098f61d61fdfbaa148710cd242d0ef5ae94fe
Bennjmann <Bennjmann.red@gmail.com>
2021-06-03 17:31:31 +1000

    Added Comments to RandomXposition.cs.

--- 2.3D_PlatFormer/Assets/_Scripts/Tiles And Backgrounds/Tilespecific/RandomXposition.cs
+++ 2.3D_PlatFormer/Assets/_Scripts/Tiles And Backgrounds/Tilespecific/RandomXposition.cs
@@ -7,11 +7,11 @@ using Random = UnityEngine.Random;
 public class RandomXPosition : MonoBehaviour {
     public GameObject[] m_Object;
     private void Start() {
-        int numberOfObj = 0;
-        foreach (GameObject GameObj in m_Object) {
+        int numberOfObj = 0;                                                                // cycle through array.
+        foreach (GameObject GameObj in m_Object) {                                          // ForEach object in the array set to random x position.
             float random = Random.Range(-11.25f, 11.4f);
             m_Object[numberOfObj].transform.position = new Vector3(random, m_Object[numberOfObj].transform.position.y, m_Object[numberOfObj].transform.position.z);
-            Debug.Log(m_Object[numberOfObj].transform.position);
+            // Debug.Log(m_Object[numberOfObj].transform.position);
             numberOfObj++;
         }
     }
